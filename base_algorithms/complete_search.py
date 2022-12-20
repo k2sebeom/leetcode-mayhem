218104847fc4427e382c5f97ec4a656decce945b
k2sebeom <moses97@gmail.com>
2022-12-20 14:12:48 -0500

    feat: Add complete search

---
+++ base_algorithms/complete_search.py
@@ -0,0 +1,28 @@
+
+
+def all_subsets(L):
+    if len(L) == 0:
+        return [[]]
+    elif len(L) == 1:
+        return [[], L]
+    
+    subset = all_subsets(L[1:])
+    subset2 = [[L[0]] + s for s in subset]
+    return subset + subset2
+
+def all_perm(L):
+    if len(L) <= 1:
+        return [L]
+    
+    result = []
+    perms = all_perm(L[1:])
+    s = L[0]
+    for p in perms:
+        for i in range(len(p) + 1):
+            result.append(p[:i] + s + p[i:])
+    return result
+
+
+if __name__ == "__main__":
+    print(all_subsets([1, 2, 3,4 ,5]))
+    print(all_perm('absd'))
